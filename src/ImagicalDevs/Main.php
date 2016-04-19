dc4bd8fac337def17a7be9e29266ba3737e4f74d
Professor Taki <Taki21@users.noreply.github.com>
2016-04-19 17:05:33 -0400

    [W.I.P] Added Some API Functions! Think Of More to Add!

--- src/ImagicalDevs/Main.php
+++ src/ImagicalDevs/Main.php
@@ -4,8 +4,8 @@ namespace ImagicalDevs;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat as C;
-use pocketmine\utils\Config;
 use pocketmine\level\Level;
+use pocketmine\utils\Config;
 use pocketmine\level\Position;
 use pocketmine\math\Vector3;
 use pocketmine\Player;
@@ -16,11 +16,51 @@ class Main extends PluginBase {
   
   public function onEnable() {
     $this->registerPluginEvents();
+    $this->printOut(C::GREEN."[MagicalAPI] Simple API Enabled!");
+    
+    $this->makeConfigDir();
+    $this->config = new Config($this->getDataFolder(). "config.yml", Config::YAML);
+  }
+  
+  public function onDisable() {
+    $this->saveResource("config.yml");
+    $this->printOut(C::RED."[MagicalAPI] Simple API Disabled!");
   }
   
   // API Functions
+  
   function registerPluginEvents() {
     $ev = $this->getServer()->getPluginManager()->registerEvents($this,$this);
     return $ev;
   }
+  
+  function printOut() :string {
+    $msg = $this->getLogger()->info();
+    return $msg;
+  }
+  
+  function getAllPlayers() {
+    $allplayers = $this->getServer()->getOnlinePlayers();
+    return $allplayers;
+  }
+  
+  function getPlayersInLevel() {
+    $players = $this->getLevel()->getPlayers();
+    return $players;
+  }
+  
+  function makeConfigDir() {
+    $mkdir = @mkdir($this->getDataFolder());
+    return $mkdir;
+  }
+  
+  function getXYZ() {
+    $xyz = array (
+      $this->getX();
+      $this->getY();
+      $this->getZ();
+    );
+    return $xyz;
+  }
+  
 }
