dc4a5e30e5bd1c4652fd7b7bbaac3105e7df9d4d
Professor Taki <Taki21@users.noreply.github.com>
2016-04-19 16:53:32 -0400

    Update Main.php

--- src/ImagicalDevs/Main.php
+++ src/ImagicalDevs/Main.php
@@ -3,16 +3,24 @@
 namespace ImagicalDevs;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\utils\TextFormat as C;
 use pocketmine\utils\Config;
 use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\math\Vector3;
 use pocketmine\Player;
 use pocketmine\Server;
-use pocketmine\event\block\BlockBreakEvent;
-use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\Listener;
 
 class Main extends PluginBase {
+  
+  public function onEnable() {
+    $this->registerPluginEvents();
+  }
+  
+  // API Functions
+  function registerPluginEvents() {
+    $ev = $this->getServer()->getPluginManager()->registerEvents($this,$this);
+    return $ev;
+  }
 }
