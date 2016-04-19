9c209fa9d1bdfcefd8d0e0a6414102b539db7d3d
Professor Taki <Taki21@users.noreply.github.com>
2016-04-19 16:27:33 -0400

    Create Main.php

---
+++ src/ImagicalDevs/Main.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ImagicalDevs;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\utils\Config;
+use pocketmine\level\Level;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+
+class Main extends PluginBase {
+}
